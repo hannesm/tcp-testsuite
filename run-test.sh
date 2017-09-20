2ae9aa6d97ba1876719e1f75b5e86a248252143a
Hannes Mehnert <hannes@mehnert.org>
2017-09-20 14:35:13 +0100

    slightly better run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -3,7 +3,11 @@
 packetdrill=/home/hannes/devel/packetdrill/gtests/net/packetdrill/packetdrill
 timelimit=10s
 
-timeout $timelimit $packetdrill $1 #> $1.out 2>&1
+echo "starting up dtrace"
+sudo /home/hannes/devel/tcptest/run_dtrace.sh &
+sleep 1
+
+sudo timeout $timelimit $packetdrill $1 > $1.out 2>&1
 result=$?
 if [ $result -eq 0 ] ; then
     rm $1.out
@@ -11,3 +15,13 @@ if [ $result -eq 0 ] ; then
 else
     printf "FAILED\n"
 fi
+
+sleep 2
+echo "hextotrace"
+/home/hannes/devel/tcptest/hextotrace.native trace.out
+echo $?
+sudo rm trace.out
+filename=$(echo $1 | cut -d '/' -f 3)
+echo "moving to $filename.trace"
+mv trace.out.tmp $filename.trace
+echo "done with $?"
